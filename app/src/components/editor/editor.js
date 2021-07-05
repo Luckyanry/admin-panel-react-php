bbdbbebb3289641ed1f69fc06d5e47a835cbd111
Andrii Kochmaruk <luckyanry@gmail.com>
2021-07-05 20:51:43 +0200

    Created a visual editor, enable the implementation of elements using contentEditable = true, work with a copy of the DOM witch was written into temp.html using XMLSerializer, DOMParser and virtualDom js-methods

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
 import axios from "axios";
+import "../../helpers/iframeLoader.js";
 
 export default class Editor extends Component {
   constructor() {
     super();
 
+    this.currentPage = "index.html";
     this.state = {
       pageList: [],
       newPageName: "",
@@ -14,7 +16,87 @@ export default class Editor extends Component {
   }
 
   componentDidMount() {
+    this.init(this.currentPage);
+  }
+
+  init(page) {
+    this.iframe = document.querySelector("iframe");
+    this.open(page);
     this.loadPageList();
+  }
+
+  open(page) {
+    this.currentPage = `../${page}?rnd=${Math.random()}`;
+
+    axios
+      .get(`../${page}`)
+      .then((res) => this.parseStringToDOM(res.data))
+      .then(this.wrapTextNodes)
+      .then((dom) => {
+        this.virtualDom = dom;
+        return dom;
+      })
+      .then(this.serializeDOMToString)
+      .then((html) => axios.post("./api/saveTempPage.php", {html}))
+      .then(() => this.iframe.load("../temp.html"))
+      .then(() => this.enableEditing());
+  }
+
+  enableEditing() {
+    this.iframe.contentDocument.body
+      .querySelectorAll("text-editor")
+      .forEach((elem) => {
+        elem.contentEditable = "true";
+        elem.addEventListener("input", () => {
+          this.onTextEdit(elem);
+        });
+      });
+  }
+
+  onTextEdit(elem) {
+    const id = elem.getAttribute("nodeid");
+    this.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML =
+      elem.innerHTML;
+  }
+
+  parseStringToDOM(str) {
+    const parser = new DOMParser();
+    return parser.parseFromString(str, "text/html");
+  }
+
+  wrapTextNodes(dom) {
+    const body = dom.body;
+    let textNodes = [];
+
+    function recursy(elem) {
+      elem.childNodes.forEach((node) => {
+        if (
+          node.nodeName === "#text" &&
+          node.nodeValue.replace(/\s+/g, "").length > 0
+        ) {
+          textNodes.push(node);
+        } else {
+          recursy(node);
+        }
+      });
+    }
+    recursy(body);
+
+    textNodes.forEach((node, idx) => {
+      const wrapper = dom.createElement("text-editor");
+
+      node.parentNode.replaceChild(wrapper, node);
+
+      wrapper.appendChild(node);
+      wrapper.setAttribute("nodeid", idx);
+    });
+
+    return dom;
+  }
+
+  serializeDOMToString(dom) {
+    const serializer = new XMLSerializer();
+    return serializer.serializeToString(dom);
   }
 
   loadPageList() {
@@ -36,29 +118,30 @@ export default class Editor extends Component {
   }
 
   render() {
-    const {pageList} = this.state;
-    const pages = pageList.map((page, idx) => {
-      return (
-        <h1 key={idx}>
-          {page}
-          <a href="#" onClick={() => this.deletePage(page)}>
-            (x)
-          </a>
-        </h1>
-      );
-    });
+    // const {pageList} = this.state;
+    // const pages = pageList.map((page, idx) => {
+    //   return (
+    //     <h1 key={idx}>
+    //       {page}
+    //       <a href="#" onClick={() => this.deletePage(page)}>
+    //         (x)
+    //       </a>
+    //     </h1>
+    //   );
+    // });
 
     return (
-      <>
-        <input
-          type="text"
-          onChange={(e) => {
-            this.setState({newPageName: e.target.value});
-          }}
-        />
-        <button onClick={this.createNewPage}>Create New Page</button>
-        {pages}
-      </>
+      <iframe src={this.currentPage} frameBorder="0"></iframe>
+      // <>
+      //   <input
+      //     type="text"
+      //     onChange={(e) => {
+      //       this.setState({newPageName: e.target.value});
+      //     }}
+      //   />
+      //   <button onClick={this.createNewPage}>Create New Page</button>
+      //   {pages}
+      // </>
     );
   }
 }
