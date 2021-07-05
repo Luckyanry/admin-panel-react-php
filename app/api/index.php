482c17ad94a6dc1a12c0506af1c323a6e6ac7441
Andrii Kochmaruk <luckyanry@gmail.com>
2021-07-05 13:04:48 +0200

    implemented the creation of pages on the server and their processing using php

--- app/api/index.php
+++ app/api/index.php
@@ -1,2 +1,13 @@
 <?php
-echo "Hello world";
+$htmlfiles = glob("../../*.html");
+$response = [];
+
+foreach($htmlfiles as $file) {
+  array_push($response, basename($file));
+}
+/* 
+    Якщо повертаємо просто $htmlfiles - отримаємо стрічку "Array",
+    щоб отримати масив використовуємо функцію var_dump($htmlfiles),
+    щоб отримати масив у форматі json - json_encode($htmlfiles)
+*/
+echo json_encode($response);
--- app/src/main.js
+++ app/src/main.js
@@ -1,1 +1,31 @@
 import $ from "jquery";
+
+function getPageList() {
+  $("h1").remove();
+
+  $.get(
+    "./api",
+    (data) => {
+      data.forEach((el) => {
+        $("body").append(`<h1>${el}</h1>`);
+      });
+    },
+    "JSON"
+  );
+}
+
+getPageList();
+
+$("button").click(() => {
+  $.post(
+    "./api/createNewPage.php",
+    {
+      name: $("input").val(),
+    },
+    () => {
+      getPageList();
+    }
+  ).fail(() => {
+    alert("The page already exists!");
+  });
+});
